c9617d24aa06e89078ee2cd2a1d1e58e4c2f3150
Miguel Castillo Barajas <miguel.castillo@corporativo.rll>
2025-01-15 00:25:07 -0600

    change port

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ dotenv.config();
 
 var app = express();
 
+app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(json())
@@ -41,7 +42,7 @@ app.post('/updateMyLevel',async (req, res) => {
 
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
 
-    console.log(`Server is running on port 3000`);
+    console.log(`Server is running on port 8080`);
 });
